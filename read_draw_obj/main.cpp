6875fb127fdbccba1f60c341aa22d735316a1ac2
yes1989yes@163.com <HtF-L7u-E78-V5N>
2017-01-23 16:11:05 +0800

    Initial Commit

---
+++ read_draw_obj/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  read_draw_obj
+//
+//  Created by depeng LIU on 2017/1/23.
+//  Copyright © 2017年 depeng LIU. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
